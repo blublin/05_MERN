71736d65cf8cb25a23e969f1295bd7ff9bc7b3b1
blublin <lublin.ben@gmail.com>
2022-05-01 16:40:44 -0700

    initial commit ninjas

---
+++ 01_Javascript/02_OOP/Ninja/index.js
@@ -0,0 +1,21 @@
+class Ninja {
+    constructor(name, health, speed = 3, strength = 3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+    sayName() {
+        console.log(`This ninjas name is: ${ this.name }`)
+    }
+    showStats() {
+        console.log(`Ninjas name: ${ this.name }.\nNinjas strength: ${ this.strength }.\nNinjas speed: ${ this.speed }.\nNinjas health: ${ this.health}.`)
+    }
+    drinkSake() {
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
