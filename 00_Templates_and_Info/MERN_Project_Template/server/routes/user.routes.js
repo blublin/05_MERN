1de304fe350fe413fe96ce4f21169019fdbefdba
blublin <lublin.ben@gmail.com>
2022-05-14 21:10:14 -0700

    updated template routes

--- 00_Templates_and_Info/MERN_Project_Template/server/routes/user.routes.js
+++ 00_Templates_and_Info/MERN_Project_Template/server/routes/user.routes.js
@@ -1,9 +1,9 @@
 const UserController = require("../controllers/user.controller");
 
 module.exports = app => {
-  app.get("/api/users/", UserController.findAllUsers);
-  app.get("/api/users/:id", UserController.findOneSingleUser);
-  app.put("/api/users/update/:id", UserController.updateExistingUser);
-  app.post("/api/users/new", UserController.createNewUser);
-  app.delete("/api/users/delete/:id", UserController.deleteAnExistingUser);
+  app.get(    "/api/users/",    UserController.findAllUsers);
+  app.post(   "/api/users",     UserController.createNewUser);
+  app.get(    "/api/users/:id", UserController.findOneSingleUser);
+  app.put(    "/api/users/:id", UserController.updateExistingUser);
+  app.delete( "/api/users/:id", UserController.deleteAnExistingUser);
 };
