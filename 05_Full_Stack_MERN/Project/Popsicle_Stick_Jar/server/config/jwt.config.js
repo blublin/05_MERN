7c324dcb747cb2a64c898e6efeb8489daec0296d
blublin <lublin.ben@gmail.com>
2022-05-26 21:09:38 -0700

    PSJ: Added debug messages to authentication

--- 05_Full_Stack_MERN/Project/Popsicle_Stick_Jar/server/config/jwt.config.js
+++ 05_Full_Stack_MERN/Project/Popsicle_Stick_Jar/server/config/jwt.config.js
@@ -5,7 +5,8 @@ const DEBUG = true;
 module.exports.authenticate = (req, res, next) => {
     DEBUG && console.log("Cookie:", req.cookies)
     jwt.verify(req.cookies.freshPopsicle, process.env.JWT_KEY , (err, payload) => {
-        console.log(err)
+        DEBUG && console.log("Authentication Error:", err)
+        DEBUG && console.log("Authentication Payload:", payload)
         err
             ? res.status(401).json({verified: false})
             : next()
